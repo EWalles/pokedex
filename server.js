33cc0e488603133595b05cbe670fe838209b137d
EWalles <ejomcdonald@gmail.com>
2022-09-01 14:42:50 -0400

    Initial setup

---
+++ server.js
@@ -0,0 +1,38 @@
+// REQUIRE DEPENDENCIES
+
+
+// INITIALIZE EXPRESS APP
+
+
+//MIDDLEWARE
+
+
+//DEFINE ROUTE
+
+
+//INDEX
+
+
+//NEW
+
+
+//DELETE
+
+
+//UPDATE
+
+
+//CREATE
+
+
+//EDIT
+
+
+//SHOW
+
+
+//TELL OUR APP TO LISTEN
+app.listen(PORT, function() {
+    console.log("Pokedex App is listening to port " + PORT + ".");
+  });
+  
