74153ec26a9ae09177281ef7e4cb16280835e24b
EWalles <ejomcdonald@gmail.com>
2022-09-01 14:58:27 -0400

    server is working

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 // REQUIRE DEPENDENCIES
 
+const express    = require('express');
+const app        = express();
+const methodOverride = require('method-override');
+const Pokemon    = require('./models/pokemon.js');
+const PORT    = process.env.PORT || 3000;
 
-// INITIALIZE EXPRESS APP
-
-
-//MIDDLEWARE
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
 
 //DEFINE ROUTE
-
+app.get('/', function(req, res) {
+    res.render('index.ejs', {
+      data: Pokemon
+    });
+  });
 
 //INDEX
 
@@ -30,7 +37,12 @@
 
 //SHOW
 
-
+app.get('/:index', function(req, res) {
+    res.render('show.ejs', {
+      data: Pokemon[req.params.index],
+      index: req.params.index
+    });
+  });
 //TELL OUR APP TO LISTEN
 app.listen(PORT, function() {
     console.log("Pokedex App is listening to port " + PORT + ".");
